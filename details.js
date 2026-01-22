c8055d8bc14b994290be75747d82dca45b4cf76d
Dedes <federico.lepore73@gmail.com>
2026-01-22 17:58:19 +0100

    details.js

--- details.js
+++ details.js
@@ -1,17 +1,3 @@
-document.onload = function () {
-    const params = new URLSearchParams(window.location.search);
-    const imgId = params.get('id');
-    const imgSrc = params.get('src');
-    const imgAlt = params.get('alt');
-
-    const detailImage = document.getElementById('detail-image');
-    const detailImageAlt = document.getElementById('detail-image-alt');
-
-    detailImage.src = imgSrc;
-    detailImage.alt = imgAlt;
-    detailImageAlt.textContent = `Image ID: ${imgId}`;
-}
-
 const url = location.search // tutto il contenuto della barra degli indirizzi
 console.log('url', url)
 const allTheParameters = new URLSearchParams(url) // un oggetto con dentro tutti i parametri
