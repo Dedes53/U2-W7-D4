b609d5ccc363b1a790c406ad64d02223b5dfb1a9
Dedes <federico.lepore73@gmail.com>
2026-01-22 15:41:06 +0100

    load imgs

--- pexels.js
+++ pexels.js
@@ -1,6 +1,8 @@
 // ci colleghiamo all'api tramite la nostra key 7MnXIBqablYv6DtTQYdoj61wgpzbwmKcp2OOiWqdwLJWo8rFHFUEFl36
-const pexelsURL = 'https://api.pexels.com/v1/search?query=kittens';
+const pexelsURL = 'https://api.pexels.com/v1/search?query=fish';
 const APIkey = "7MnXIBqablYv6DtTQYdoj61wgpzbwmKcp2OOiWqdwLJWo8rFHFUEFl36";
+
+const imgs = document.getElementsByTagName('img');
 
 fetch(pexelsURL, {
     headers: {
@@ -17,6 +19,9 @@ fetch(pexelsURL, {
     })
     .then((data) => {
         console.log(data);
+        for (let i = 0; i < imgs.length; i++) {
+            imgs[i].src = data.photos[i].src.medium;
+        }
     })
     .catch((err) => {
         console.error('Errore:', err);
